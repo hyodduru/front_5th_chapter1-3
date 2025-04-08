f35a73b90bdf2cd44e0c48213a6923c5cfc9348f
hyodduru <hoje15v@gmail.com>
2025-04-08 23:20:02 +0900

    feat: 얕은/깊은 객체 비교 함수(shallowEquals, deepEquals) 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,36 @@
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (
+    typeof objA !== "object" ||
+    typeof objB !== "object" ||
+    objA === null ||
+    objB === null
+  ) {
+    return objA === objB;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) return false;
+    return objA.every((x, i) => deepEquals(x, objB[i]));
+  }
+
+  const keys = new Set([...Object.keys(objA), ...Object.keys(objB)]);
+
+  for (const key of keys) {
+    const valueA = objA[key as keyof T];
+    const valueB = objB[key as keyof T];
+
+    const isBothObjects =
+      typeof valueA === "object" &&
+      typeof valueB === "object" &&
+      valueA !== null &&
+      valueB !== null;
+
+    if (isBothObjects) {
+      if (!deepEquals(valueA, valueB)) return false;
+    } else {
+      if (valueA !== valueB) return false;
+    }
+  }
+
+  return true;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,27 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (
+    typeof objA !== "object" ||
+    typeof objB !== "object" ||
+    objA === null ||
+    objB === null
+  ) {
+    return objA === objB;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) return false;
+
+    return objA.every((x, i) => x === objB[i]);
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) return false;
+
+  for (const key of keysA) {
+    if (!(key in objB)) return false;
+    if (objA[key as keyof T] !== objB[key as keyof T]) return false;
+  }
+  return true;
 }
