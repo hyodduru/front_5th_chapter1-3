73464592a46fc4931f0a3c61530106c248b613ec
hyodduru <hoje15v@gmail.com>
2025-04-09 00:45:34 +0900

    feat: props 비교로 리렌더링 방지하는 커스텀 memo HOC 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, useRef, ReactElement, createElement } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  return function Memoized(props: P): ReactElement {
+    const prevPropsRef = useRef<P | null>(null);
+    const renderedRef = useRef<ReactElement | null>(null);
+
+    const isSame = prevPropsRef.current && _equals(prevPropsRef.current, props);
+
+    if (isSame && renderedRef.current) {
+      return renderedRef.current;
+    }
+
+    const rendered = createElement(Component, props);
+    renderedRef.current = rendered;
+    prevPropsRef.current = props;
+
+    return rendered;
+  };
 }
