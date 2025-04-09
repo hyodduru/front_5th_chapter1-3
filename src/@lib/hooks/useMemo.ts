a0af03e89da339a059bb72fea6d32cb6f2a64726
hyodduru <hoje15v@gmail.com>
2025-04-09 23:15:32 +0900

    feat: hooks 구현 (useRef, useMemo, useCallback)

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,12 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
 export function useMemo<T>(
   factory: () => T,
   _deps: DependencyList,
   _equals = shallowEquals,
 ): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  const memoRef = useRef<{ deps: DependencyList; value: T } | null>(null);
+
+  if (memoRef.current === null || !_equals(memoRef.current.deps, _deps)) {
+    const value = factory();
+    memoRef.current = { deps: [..._deps], value };
+    return value;
+  }
+
+  return memoRef.current.value;
 }
